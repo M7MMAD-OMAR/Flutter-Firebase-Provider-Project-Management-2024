82211807d2d83a024cacc92458cb0c8d3218698f
MUHMAD OMAR <muhammadaaa727@gmail.com>
2024-06-07 02:37:35 +0300

    Fix app and add models

---
+++ src/constants/values/values.dart
@@ -0,0 +1,23 @@
+library values;
+
+import 'dart:math';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import '../../models/User/User_model.dart';
+import '../../models/team/Team_model.dart';
+import '../../widgets/dummy/profile_dummy.dart';
+import '../../widgets/stacked_images.dart';
+
+part './app_colors.dart';
+
+part './styles.dart';
+
+part './button_styles.dart';
+
+part '../../../src/utils/utils.dart';
+
+part './box_decoration_styles.dart';
+
+part './spacing.dart';
--- src/routes.dart
+++ src/routes.dart
@@ -6,7 +6,11 @@ class Routes {
 
   // static variables
   static const String homeScreen = '/home-screen';
+  static const String authScreen = '/auth-screen';
+
+  // route list
   static final dynamic routes = <String, WidgetBuilder>{
     homeScreen: (BuildContext context) => const HomeScreen(),
+    // authScreen: (BuildContext context) => const AuthScreen(),
   };
 }
