8f5564f10e6826bb97353cb4b0ab4ca39536ff31
MUHMAD OMAR <muhammadaaa727@gmail.com>
2024-06-16 03:38:58 +0300

    Fix

--- android/app/src/main/kotlin/com/asalib/project_management_muhmad_omar/MainActivity.kt
+++ android/app/src/main/kotlin/com/asalib/project_management_muhmad_omar/MainActivity.kt
@@ -2,5 +2,4 @@ package com.asalib.project_management_muhmad_omar
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
